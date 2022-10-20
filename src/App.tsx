d0a215a4009fcf9556e6e403a545067fc7c3ce01
Duy Pham <duypham9895@gmail.com>
2022-10-20 15:01:50 +0700

    Initial animate on scroll library

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 type Props = {};
 
 const App = (props: Props) => {
+  useEffect(() => {
+    AOS.init({ duration: 1200 });
+    AOS.refresh();
+  }, []);
   return (
     <>
       <h2>Hello world</h2>
